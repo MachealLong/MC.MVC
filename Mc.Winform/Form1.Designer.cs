c582059c463862ca165589104f39f9da4a968218
MachealLong <c_longk@sina.cn>
2018-11-12 22:42:20 +0800

    测试图片合成

---
+++ Mc.Winform/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Mc.Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "合并图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(273, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(89, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(161, 123);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(335, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(161, 123);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(596, 50);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(161, 123);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Mc.Winform/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace Mc.Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Image<Rgba32> imageone = null;
+
+        public Image<Rgba32> imagetwo = null;
+
+        IImageFormat format = null;
+
+
+
+        /// <summary>
+        /// 合并图片 小图片放在大图片上面
+        /// </summary>
+        /// <param name="TempleBase64Str">模板大图片base64</param>
+        /// <param name="OutputBase64Str">模板小图片base64</param>
+        /// <param name="x">X坐标</param>
+        /// <param name="y">y坐标</param>
+        /// <returns></returns>
+        public ImageResponse MergeImage(string TempleBase64Str, string OutputBase64Str, int x, int y)
+        {
+            string strRet = null;
+            //if (string.IsNullOrEmpty(TempleBase64Str))
+            //{
+            //    return new ImageResponse { success = false, errmsg = "请传入模板大图片base64" };
+            //}
+            //if (string.IsNullOrEmpty(OutputBase64Str))
+            //{
+            //    return new ImageResponse { success = false, errmsg = "请传入模板小图片base64" };
+            //}
+            if (x < 0 || y < 0)
+            {
+                return new ImageResponse { success = false, errmsg = "坐标不能传入负数" };
+            }
+            try
+            {
+                //byte[] templebytes = Convert.FromBase64String(TempleBase64Str);
+                //byte[] outputbytes = Convert.FromBase64String(OutputBase64Str);
+               
+                var imagesTemle = imageone;
+                //SixLabors.ImageSharp.Image.Load(templebytes, out format);
+                var outputImg = imagetwo;
+                    //SixLabors.ImageSharp.Image.Load(outputbytes);
+
+                if (imagesTemle.Height - (outputImg.Height + y) <= 0)
+                {
+                    return new ImageResponse { success = false, errmsg = "Y坐标高度超限" };
+                }
+                if (imagesTemle.Width - (outputImg.Width + x) <= 0)
+                {
+                    return new ImageResponse { success = false, errmsg = "X坐标宽度超限" };
+                }
+                //进行多图片处理
+                imagesTemle.Mutate(a =>
+                {
+                    //还是合并 
+                    a.DrawImage(outputImg, 1, new SixLabors.Primitives.Point(x, y));
+                });
+                strRet = imagesTemle.ToBase64String(format);
+                //imagesTemle.SaveAsPng();
+                byte[] arr = Convert.FromBase64String(strRet);
+                MemoryStream ms = new MemoryStream(arr);
+                Bitmap bmp = new Bitmap(ms);
+                pictureBox3.Image = bmp;
+                return new ImageResponse { success = true, base64Str = strRet };
+            }
+            catch (Exception ex)
+            {
+                return new ImageResponse { success = false, errmsg = "报错信息" + ex.Message };
+            }
+        }
+
+        private void Base64StringToImage(string txtFileName)
+        {
+            try
+            {
+                FileStream ifs = new FileStream(txtFileName, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(ifs);
+
+                String inputStr = sr.ReadToEnd();
+                byte[] arr = Convert.FromBase64String(inputStr);
+                MemoryStream ms = new MemoryStream(arr);
+                Bitmap bmp = new Bitmap(ms);
+
+                //bmp.Save(txtFileName + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                //bmp.Save(txtFileName + ".bmp", ImageFormat.Bmp);
+                //bmp.Save(txtFileName + ".gif", ImageFormat.Gif);
+                //bmp.Save(txtFileName + ".png", ImageFormat.Png);
+                ms.Close();
+                sr.Close();
+                ifs.Close();
+                this.pictureBox2.Image = bmp;
+                if (File.Exists(txtFileName))
+                {
+                    File.Delete(txtFileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Base64StringToImage 转换失败/nException：" + ex.Message);
+            }
+        }  
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result=MergeImage("","",100,100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"C:";
+            ofd.Title = "打开文件";
+            if (ofd.ShowDialog()==DialogResult.OK) {
+                imageone= SixLabors.ImageSharp.Image.Load(ofd.FileName,out format);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"C:";
+            ofd.Title = "打开文件";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                imagetwo = SixLabors.ImageSharp.Image.Load(ofd.FileName);
+            }
+
+        }
+    }
+
+    public class ImageResponse {
+        public bool success { set; get; }
+        public string base64Str { set; get; }
+        public string errmsg { set; get; }
+    }
+
+
+}
